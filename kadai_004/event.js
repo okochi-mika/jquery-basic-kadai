7c80df9bb06a3a821d7c3b5eca2fc9813bd380ff
okochi-mika <hiyoko_m@icloud.com>
2025-04-04 15:19:14 +0900

    コードを修正

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-       console.log('loadイベントが発生しました');
-   });
+$(window).on('load', function(){ 
+    console.log('loadイベントが発生しました'); 
+});
 
    $(window).scroll(function(){
     console.log('scrollイベントが発生しました');
